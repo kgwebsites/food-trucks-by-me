59c83bbd65ab64b105aafb897d49a240607a1623
Kyle Goss <kyle@kgwebsites.com>
2019-06-01 07:31:16 -0700

    !

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,7 @@ import { TruckContext } from '../contexts/TruckContext';
 
 function Error() {
   const { error } = useContext(TruckContext);
-  if (error) return null;
+  if (!error) return null;
   return <div>Error: {error}</div>;
 }
 
