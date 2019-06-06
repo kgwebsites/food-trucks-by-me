468e5ab85cae2234ab5167ea00a1d3e544a5078d
Kyle Goss <kyle@kgwebsites.com>
2019-06-05 21:56:10 -0700

    logging

--- functions/get_address_from_coor.js
+++ functions/get_address_from_coor.js
@@ -2,6 +2,7 @@ const fetchAddress = require('./core/fetchAddress');
 
 exports.handler = function(event, context, callback) {
   const { lat, lng } = JSON.parse(event.body);
+  console.log('handler event.body', 'lat', lat, 'lng', lng);
   fetchAddress(lat, lng)
     .then(address => {
       callback(null, {
