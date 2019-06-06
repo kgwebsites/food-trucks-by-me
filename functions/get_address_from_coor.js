e1d21ba83876f3a609f6082ac6dfd9ae0049b00d
Kyle Goss <kyle@kgwebsites.com>
2019-06-05 22:33:22 -0700

    more logs...

--- functions/get_address_from_coor.js
+++ functions/get_address_from_coor.js
@@ -4,12 +4,14 @@ exports.handler = function(event, context, callback) {
   const { lat, lng } = JSON.parse(event.body);
   fetchAddress(lat, lng)
     .then(address => {
+      console.log('address', address);
       callback(null, {
         statusCode: 200,
         body: address,
       });
     })
     .catch(err => {
+      console.log('err', err);
       callback(err);
     });
 };
