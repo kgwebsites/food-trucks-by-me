c7a982badc0241adc7889238b26137f54faad8b1
Kyle Goss <kyle@kgwebsites.com>
2019-06-05 21:42:27 -0700

    address

--- functions/get_address_from_coor.js
+++ functions/get_address_from_coor.js
@@ -1,10 +1,9 @@
-// require('dotenv').config();
 const fetchAddress = require('./core/fetchAddress');
 
 exports.handler = function(event, context, callback) {
   const { lat, lng } = JSON.parse(event.body);
   fetchAddress(lat, lng)
-    .then(({ address }) => {
+    .then(address => {
       callback(null, {
         statusCode: 200,
         body: address,
