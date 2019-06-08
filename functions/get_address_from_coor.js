1498e5937c66890b20057391d7444d7c3f162129
Kyle Goss <kyle@kgwebsites.com>
2019-06-07 20:58:04 -0700

    Does async/await work in netlify functions? Lets find out

--- functions/get_address_from_coor.js
+++ functions/get_address_from_coor.js
@@ -1,15 +1,14 @@
 const fetchAddress = require('./core/fetchAddress');
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   const { lat, lng } = JSON.parse(event.body);
-  fetchAddress(lat, lng)
-    .then(address => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(address),
-      });
-    })
-    .catch(err => {
-      callback(err);
+  try {
+    const address = await fetchAddress(lat, lng);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(address),
     });
+  } catch (err) {
+    callback(err);
+  }
 };
--- functions/get_food_trucks.js
+++ functions/get_food_trucks.js
@@ -1,33 +1,31 @@
 const fetchLongLat = require('./core/fetchLongLat');
 const filterFoodTrucks = require('./core/filterFoodTrucks');
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   const { address, range, day, start24, end24 } = JSON.parse(event.body);
-  fetchLongLat(address)
-    .then(({ lat, lng }) => {
-      if (lng && lat) {
-        filterFoodTrucks({
-          lng,
-          lat,
-          range,
-          day,
-          start24,
-          end24,
-        })
-          .then(response => {
-            callback(null, {
-              statusCode: 200,
-              body: JSON.stringify({ trucks: response, lng, lat }),
-            });
-          })
-          .catch(() => {
-            throw new Error('Something went wrong, try again later');
-          });
-      } else {
-        throw new Error('Invalid address');
+  try {
+    const { lat, lng } = await fetchLongLat(address);
+    if (lng && lat) {
+      const response = await filterFoodTrucks({
+        lng,
+        lat,
+        range,
+        day,
+        start24,
+        end24,
+      });
+      try {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ trucks: response, lng, lat }),
+        });
+      } catch {
+        throw new Error('Something went wrong, try again later');
       }
-    })
-    .catch(err => {
-      callback(err);
-    });
+    } else {
+      throw new Error('Invalid address');
+    }
+  } catch (err) {
+    callback(err);
+  }
 };
